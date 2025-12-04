697e5a366ad8bfb0f0d49a3c2e2eba53824f3235
sepiroth-x <chardy.tsadiq@gmail.com>
2025-12-05 00:36:39 +0800

    Fix UpdateSystem UI: Remove double 'v' prefix in version display - Change 'VantaPress vv1.0.16' to 'VantaPress v1.0.16' - Version variable already includes 'v' prefix from config

--- resources/views/filament/pages/update-system.blade.php
+++ resources/views/filament/pages/update-system.blade.php
@@ -9,7 +9,7 @@
             <div class="flex items-center justify-between">
                 <div>
                     <h3 class="text-2xl font-bold text-gray-900 dark:text-white">
-                        VantaPress v{{ $currentVersion }}
+                        VantaPress {{ $currentVersion }}
                     </h3>
                     <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
                         Your current installation version
